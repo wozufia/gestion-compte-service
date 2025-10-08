f9a8144cd2209eb0c14b68c275677d0994fad133
wozufia <kofwoz@yahoo.fr>
2025-10-08 17:17:54 -0400

    1er commit

---
+++ src/main/java/com/awa/centrale/gestioncompte/GestionCompteApplication.java
@@ -0,0 +1,13 @@
+package com.awa.centrale.gestioncompte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCompteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCompteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/awa/centrale/gestioncompte/GestionCompteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awa.centrale.gestioncompte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCompteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
